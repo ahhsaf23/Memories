42db546c1b335614fab2ac160d21ec9dd714d7f0
ahhsaf23 <ahhsaf23@gmail.com>
2021-08-26 21:14:34 +0530

    Server

---
+++ api/routes/categories.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Category = require('../models/Category');
+
+
+//Create Category
+router.post("/", async(req, res) => {
+    const newCat= new Category(req.body);
+    
+    try{
+        const savedCat= await newCat.save();
+        res.status(200).json(savedCat);
+
+    }catch(err) {
+        res.status(500).json(err);
+    }
+   
+});
+
+//fetch all Category
+router.get("/", async(req, res) => {    
+    try{
+        const categories=await Category.find();
+        res.status(200).json(categories);
+
+    }catch(err) {
+        res.status(500).json(err);
+    }
+   
+});
+module.exports=router;
